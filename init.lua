c376bb665b62317a45f560f501d6b1577f87eb30
Cristopher Robledo <cristopherrobledo@MacBook-Pro-de-Cristopher.local>
2025-08-19 14:51:16 -0600

    -a

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
+-- Fuerza itálicas y negritas (para temas que no las definen)
+vim.cmd([[
+  hi! Italic gui=italic cterm=italic
+  hi! Bold gui=bold cterm=bold
+  syntax enable
+]])
+
 require("core")
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,14 +1,14 @@
 -- Configuración inicial de Lazy.nvim (gestor de plugins)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- versión estable
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- versión estable
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -50,16 +50,26 @@ vim.opt.wrapmargin = 0
 
 -- Atajos de teclado
 local keymap = vim.keymap
-keymap.set('n', '<F2>', ':w<CR>', { desc = 'Guardar archivo' })
-keymap.set('n', '<F3>', ':q<CR>', { desc = 'Cerrar ventana' })
-keymap.set('n', '<F4>', ':wq<CR>', { desc = 'Guardar y salir' })
-keymap.set('v', '<C-c>', '"+y', { desc = 'Copiar al portapapeles' })
-keymap.set('n', '<C-v>', '"+p', { desc = 'Pegar desde portapapeles' })
-keymap.set('i', '<C-v>', '<C-r>+', { desc = 'Pegar en modo inserción' })
-keymap.set('n', '<Tab>', ':bnext<CR>', { desc = 'Siguiente buffer' })
-keymap.set('n', '<S-Tab>', ':bprevious<CR>', { desc = 'Buffer anterior' })
-keymap.set('n', 'Q', '<nop>')
-keymap.set('n', '<C-z>', '<nop>', { desc = 'Evita suspender Neovim' })
+keymap.set("n", "<F2>", ":w<CR>", { desc = "Guardar archivo" })
+keymap.set("n", "<F3>", ":q<CR>", { desc = "Cerrar ventana" })
+keymap.set("n", "<F4>", ":wq<CR>", { desc = "Guardar y salir" })
+keymap.set("v", "<C-c>", '"+y', { desc = "Copiar al portapapeles" })
+keymap.set("n", "<C-v>", '"+p', { desc = "Pegar desde portapapeles" })
+keymap.set("i", "<C-v>", "<C-r>+", { desc = "Pegar en modo inserción" })
+keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Siguiente buffer" })
+keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "Buffer anterior" })
+keymap.set("n", "Q", "<nop>")
+keymap.set("n", "<C-z>", "<nop>", { desc = "Evita suspender Neovim" })
+
+-- Bloqueo de flechas de navegación
+keymap.set("n", "<Up>", "<nop>", { desc = "Desactivar flecha arriba" })
+keymap.set("n", "<Down>", "<nop>", { desc = "Desactivar flecha abajo" })
+keymap.set("n", "<Left>", "<nop>", { desc = "Desactivar flecha izquierda" })
+keymap.set("n", "<Right>", "<nop>", { desc = "Desactivar flecha derecha" })
+keymap.set("i", "<Up>", "<nop>", { desc = "Desactivar flecha arriba" })
+keymap.set("i", "<Down>", "<nop>", { desc = "Desactivar flecha abajo" })
+keymap.set("i", "<Left>", "<nop>", { desc = "Desactivar flecha izquierda" })
+keymap.set("i", "<Right>", "<nop>", { desc = "Desactivar flecha derecha" })
 
 -- Transparencia
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -69,142 +79,144 @@ vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
 
 -- Plugins Mínimos
 require("lazy").setup({
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "python", "lua", "javascript", "typescript" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", { desc = "Buscar archivos" })
-    end,
-  },
-  {
-    "sainnhe/sonokai",
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_transparent_background = 1
-      vim.g.sonokai_enable_italic = 1
-      vim.g.sonokai_style = "andromeda"
-      vim.cmd.colorscheme("sonokai")
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "python", "lua", "javascript", "typescript" },
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", { desc = "Buscar archivos" })
+		end,
+	},
+	{
+		"sainnhe/sonokai",
+		priority = 1000,
+		config = function()
+			vim.g.sonokai_transparent_background = 1
+			vim.g.sonokai_enable_italic = 1
+			vim.g.sonokai_style = "andromeda"
+			vim.cmd.colorscheme("sonokai")
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Configuración para Python (Pyright)
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        settings = {
-          python = {
-            analysis = {
-              typeCheckingMode = "basic",
-              diagnosticMode = "workspace",
-              inlayHints = {
-                variableTypes = true,
-                functionReturnTypes = true,
-                parameterNames = true,
-              },
-            },
-          },
-        },
-      })
+			-- Configuración para Python (Pyright)
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				settings = {
+					python = {
+						analysis = {
+							typeCheckingMode = "basic",
+							diagnosticMode = "workspace",
+							inlayHints = {
+								variableTypes = true,
+								functionReturnTypes = true,
+								parameterNames = true,
+							},
+						},
+					},
+				},
+			})
 
-      -- Configuración para TypeScript (ts_ls)
-      lspconfig.tsserver.setup({ enabled = false })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-        on_attach = function(client)
-          client.server_capabilities.documentFormattingProvider = false
-        end,
-      })
+			-- Configuración para TypeScript (ts_ls)
+			lspconfig.tsserver.setup({ enabled = false })
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				on_attach = function(client)
+					client.server_capabilities.documentFormattingProvider = false
+				end,
+			})
 
-      -- Configuración para Lua
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            runtime = { version = 'LuaJIT' },
-            diagnostics = { globals = {'vim'} },
-            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-            telemetry = { enable = false },
-          }
-        }
-      })
-    end,
-  },
-  {
-    "windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup({}) end,
-  },
-  {
-    "stevearc/conform.nvim",
-    config = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          python = { "black", "isort" },
-          lua = { "stylua" },
-          javascript = { "prettier" },
-          typescript = { "prettier" },
-        },
-      })
-    end,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("ibl").setup({
-        indent = { char = "│" },
-        scope = { show_start = false, show_end = false },
-      })
-    end
-  },
-  -- Plugin lualine.nvim (añadido)
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "sonokai",
-          component_separators = { left = "│", right = "│" },
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = { "NvimTree", "alpha" },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { { "filename", path = 1 } },
-          lualine_x = { "encoding", "fileformat", "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = { "location" }
-        },
-        extensions = { "fugitive", "nvim-tree" },
-      })
-    end,
-  },
+			-- Configuración para Lua
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+						telemetry = { enable = false },
+					},
+				},
+			})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					python = { "black", "isort" },
+					lua = { "stylua" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+				},
+			})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("ibl").setup({
+				indent = { char = "│" },
+				scope = { show_start = false, show_end = false },
+			})
+		end,
+	},
+	-- Plugin lualine.nvim (añadido)
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "sonokai",
+					component_separators = { left = "│", right = "│" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = { "NvimTree", "alpha" },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { { "filename", path = 1 } },
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+				extensions = { "fugitive", "nvim-tree" },
+			})
+		end,
+	},
 })
 
 -- Configuración de autocompletado
@@ -215,75 +227,75 @@ local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
-  }),
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+	}),
 })
 
 -- Configuración LSP
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    local opts = { buffer = args.buf, silent = true }
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		local opts = { buffer = args.buf, silent = true }
 
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
-    if client.supports_method("textDocument/hover") then
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    end
-    
-    if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
-    end
+		if client.supports_method("textDocument/hover") then
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		end
 
-    if client.name == "pyright" then
-      vim.lsp.inlay_hint.enable(args.buf, true)
-    end
-  end,
+		if client.supports_method("textDocument/formatting") then
+			vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+		end
+
+		if client.name == "pyright" then
+			vim.lsp.inlay_hint.enable(args.buf, true)
+		end
+	end,
 })
 
 -- Autoformateo
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.py", "*.lua", "*.js", "*.ts" },
-  callback = function()
-    require("conform").format({ async = true })
-  end,
+	pattern = { "*.py", "*.lua", "*.js", "*.ts" },
+	callback = function()
+		require("conform").format({ async = true })
+	end,
 })
 
 -- Configuración adicional para snippets
 vim.keymap.set({ "i", "s" }, "<c-l>", function()
-  if luasnip.choice_active() then
-    luasnip.change_choice(1)
-  end
+	if luasnip.choice_active() then
+		luasnip.change_choice(1)
+	end
 end)
