ee16fbf8b03f6dffe5ed1fd5d34193ff85664703
Your Name <you@example.com>
2025-11-07 10:11:22 -0600

    Agregando actualizacion de copia se seguridad

---
+++ lua/plugins/cmp-ai.lua
@@ -0,0 +1,16 @@
+return {
+  'tzachar/cmp-ai',
+  dependencies = 'nvim-lua/plenary.nvim',
+  config = function()
+    local cmp_ai = require('cmp_ai.config')
+    cmp_ai:setup({
+      provider = 'OpenAI',
+      provider_options = {
+        model = 'gpt-4o-mini',
+      },
+      run_on_every_keystroke = true,
+      notify = true,
+      notify_callback = function(msg) vim.notify(msg) end,
+    })
+  end
+}
