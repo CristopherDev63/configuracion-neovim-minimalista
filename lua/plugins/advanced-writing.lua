fd570d2885d781b38d6b20c2549335ce0e7a799c
Your Name <you@example.com>
2025-12-22 18:16:59 -0600

    fix(autopairs): eliminar config duplicada e integración rota con cmp

--- lua/plugins/advanced-writing.lua
+++ lua/plugins/advanced-writing.lua
@@ -134,11 +134,6 @@ return {
 		},
 		config = function(_, opts)
 			require("nvim-autopairs").setup(opts)
-
-			-- Integración con cmp
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp = require("cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	},
 
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -50,12 +50,4 @@ return {
 			vim.api.nvim_set_hl(0, "MiniIndentscopeSymbolOff", { fg = "#75715E", bg = "NONE" }) -- Gris Molokai
 		end,
 	},
-
-	-- Auto-pares de caracteres
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	},
 }
