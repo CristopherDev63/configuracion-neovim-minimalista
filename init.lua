6806b6fd78fe9ed20d55b9c8bd5720fb37defa6a
Your Name <you@example.com>
2026-01-07 23:53:11 -0600

    fix: Configure solarized for classic look

--- init.lua
+++ init.lua
@@ -47,6 +47,12 @@ require("lazy").setup({
 		lazy = false,
 		priority = 1000,
 		config = function()
+			vim.g.solarized_italics = 1
+			vim.g.solarized_visibility = "normal"
+			vim.g.solarized_diffmode = "normal"
+			vim.g.solarized_termtrans = 0
+			vim.g.solarized_statusline = "normal"
+			vim.o.background = "dark"
 			vim.cmd("colorscheme solarized")
 		end,
 	},
