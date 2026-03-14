0841f0581a2bffb2690ad68bc98da7742731887b
Your Name <you@example.com>
2026-03-13 20:46:29 -0600

    fix(multicursor): use Alt+D instead of Ctrl+D to avoid conflict with half-page scroll

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -13,9 +13,9 @@ return {
     set({"n", "v"}, "<C-S-Up>", function() mc.lineSkipCursor(-1) end)
     set({"n", "v"}, "<C-S-Down>", function() mc.lineSkipCursor(1) end)
 
-    -- Add and remove cursors by matching word
-    set({"n", "v"}, "<C-d>", function() mc.matchAddCursor(1) end)
-    set({"n", "v"}, "<C-s>", function() mc.matchSkipCursor(1) end)
+    -- Add and remove cursors by matching word (Using Alt + d/s to avoid Ctrl-D conflict)
+    set({"n", "v"}, "<M-d>", function() mc.matchAddCursor(1) end)
+    set({"n", "v"}, "<M-s>", function() mc.matchSkipCursor(1) end)
     set({"n", "v"}, "<leader>n", function() mc.matchAddCursor(-1) end)
     set({"n", "v"}, "<leader>s", function() mc.matchSkipCursor(-1) end)
 
