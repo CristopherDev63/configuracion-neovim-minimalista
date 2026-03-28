fe6802df215e5a630939f4327d4b31b5c3de632d
Your Name <you@example.com>
2026-03-28 13:08:55 -0600

    style(bufferline): fix colors to iceberg light palette

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -26,6 +26,41 @@ return {
             },
           },
         },
+        highlights = {
+          fill = {
+            bg = "#e8e9ec",
+          },
+          background = {
+            fg = "#757a90",
+            bg = "#e8e9ec",
+          },
+          buffer_selected = {
+            fg = "#33374c",
+            bg = "#e8e9ec",
+            bold = true,
+            italic = true,
+          },
+          separator = {
+            fg = "#cbd0e0",
+            bg = "#e8e9ec",
+          },
+          separator_selected = {
+            fg = "#cbd0e0",
+            bg = "#e8e9ec",
+          },
+          indicator_selected = {
+            fg = "#2d539e",
+            bg = "#e8e9ec",
+          },
+          modified = {
+            fg = "#b36941",
+            bg = "#e8e9ec",
+          },
+          modified_selected = {
+            fg = "#b36941",
+            bg = "#e8e9ec",
+          },
+        },
       })
     end,
   },
