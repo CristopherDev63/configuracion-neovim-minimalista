4ecaea52f4c0a7c351187f572600fa864fd64231
Your Name <you@example.com>
2026-03-29 02:29:49 -0600

    feat(theme): change colorscheme to gruvbox (light mode)

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,35 @@
 return {
   {
-    "savq/melange-nvim",
+    "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.opt.background = "light"
-      vim.cmd.colorscheme("melange")
+      require("gruvbox").setup({
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, highlight groups
+        contrast = "", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+      vim.o.background = "light"
+      vim.cmd("colorscheme gruvbox")
     end,
   },
 }
