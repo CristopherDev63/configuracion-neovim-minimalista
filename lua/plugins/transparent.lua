cd48da9e4ae6b24964424647e77d209909254c50
Your Name <you@example.com>
2026-04-26 01:07:10 -0600

    Disable transparent.nvim to fix solid background

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,21 +1,8 @@
 return {
   {
     "xiyaowong/transparent.nvim",
+    enabled = false,
     lazy = false,
-    priority = 1001, -- Un poco más alto que el tema
-    config = function()
-      require("transparent").setup({
-        extra_groups = {
-          "NormalFloat",
-          "NvimTreeNormal",
-          "OilNormal",
-          "TelescopeNormal",
-          "TelescopeBorder",
-          "LspFloatWinNormal",
-        },
-      })
-      -- Activar transparencia inmediatamente
-      -- vim.cmd("TransparentEnable")
-    end,
+    priority = 1001,
   },
 }
