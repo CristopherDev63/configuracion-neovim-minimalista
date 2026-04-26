341ec3a01dd968dc2eeb66c8bf344de1462e7926
Your Name <you@example.com>
2026-04-26 00:55:31 -0600

    Change theme to nightfox.nvim

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,45 +1,43 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
-    name = "gruvbox",
+    "EdenEast/nightfox.nvim",
+    name = "nightfox",
     lazy = false,
     priority = 1000,
     config = function()
-      -- Configuración de Gruvbox (versión clásica Lua)
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
+      require("nightfox").setup({
+        options = {
+          terminal_colors = true,
+          undercurl = true,
+          underline = true,
+          bold = true,
+          italic = {
+            strings = true,
+            emphasis = true,
+            comments = true,
+            operators = false,
+            folds = true,
+          },
+          strikethrough = true,
+          invert_selection = false,
+          invert_signs = false,
+          invert_tabline = false,
+          invert_intend_guides = false,
+          inverse = true,
+          contrast = "hard",
+          palette_overrides = {},
+          overrides = {},
+          dim_inactive = false,
+          transparent_mode = false,
         },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, quiet pastels
-        contrast = "hard", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
       })
-      
-      -- Asegurar fondo oscuro y contraste hard
-      vim.o.background = "dark"
-      
-      -- Cargar el esquema de colores
-      vim.cmd([[colorscheme gruvbox]])
 
-      -- Mantener estilos personalizados de legibilidad adicionales (si hicieran falta)
+      vim.o.background = "dark"
+
+      vim.cmd([[colorscheme nightfox]])
+
       vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "gruvbox",
+        pattern = "nightfox",
         callback = function()
           vim.api.nvim_set_hl(0, "Comment", { italic = true })
           vim.api.nvim_set_hl(0, "@comment", { italic = true })
