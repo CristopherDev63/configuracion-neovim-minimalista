739039c3312b75069c10f5e349fa5a7c37539a98
Your Name <you@example.com>
2026-04-28 18:12:56 -0600

    Cambiar tema de colores de everforest a ayu dark

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,31 +1,15 @@
 return {
   {
-    "sainnhe/everforest",
+    "Shatur/neovim-ayu",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.everforest_background = "hard"
-      vim.g.everforest_transparent_background = 0
-      vim.g.everforest_enable_italic = true
-      vim.g.everforest_bold = true
-      vim.g.everforest_underline = true
-
-      vim.o.background = "dark"
-
-      vim.cmd([[colorscheme everforest]])
-
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "everforest",
-        callback = function()
-          vim.api.nvim_set_hl(0, "Comment", { italic = true })
-          vim.api.nvim_set_hl(0, "@comment", { italic = true })
-          vim.api.nvim_set_hl(0, "@variable.parameter", { italic = true })
-          vim.api.nvim_set_hl(0, "Keyword", { bold = true })
-          vim.api.nvim_set_hl(0, "@keyword", { bold = true })
-          vim.api.nvim_set_hl(0, "@keyword.function", { bold = true })
-          vim.api.nvim_set_hl(0, "@keyword.conditional", { bold = true })
-        end,
+      require('ayu').setup({
+        mirage = false,
+        theme = "dark",
+        overrides = {},
       })
+      vim.cmd([[colorscheme ayu]])
     end,
   },
 }
