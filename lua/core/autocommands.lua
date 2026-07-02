9b0f144753c5e3ea4ae12e6ec7d2e3fc5efc7139
Your Name <you@example.com>
2026-07-01 22:45:17 -0600

    feat: restaurar última posición del cursor al abrir archivos

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -71,3 +71,14 @@ vim.api.nvim_create_autocmd("FileType", {
 		end,
 	})
 
+-- Restaurar la última posición del cursor al abrir archivos
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
