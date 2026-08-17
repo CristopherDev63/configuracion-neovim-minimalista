435011b10e7a859fe157515a9ba9cd52ee8e7565
Your Name <you@example.com>
2026-08-17 09:29:22 -0600

    fix(treesitter): habilitar highlights para todos los lenguajes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,12 +6,9 @@ return {
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = { "python", "lua", "java", "javascript", "typescript", "bash", "php", "sql", "html", "css" },
-				highlight = {
-					enable = function(lang)
-						local html_langs = { "html", "css", "javascript", "typescript", "jsx", "tsx" }
-						return vim.tbl_contains(html_langs, lang)
-					end,
-					disable = function(lang, buf)
+			highlight = {
+				enable = true,
+				disable = function(lang, buf)
 						local max_filesize = 100 * 1024 -- 100 KB
 						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 						if ok and stats and stats.size > max_filesize then
